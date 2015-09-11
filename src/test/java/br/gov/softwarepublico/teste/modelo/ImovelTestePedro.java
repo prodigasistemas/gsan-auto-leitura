be299e5e2304ff3ef94b21610469e53adecdb7ca
Pedro Cesat Barros Loureiro <pedrocesar@compesa.com.br>
2015-09-11 11:23:51 -0300

    teste Pedro Compesa

---
+++ src/test/java/br/gov/softwarepublico/teste/modelo/ImovelTestePedro.java
@@ -0,0 +1,16 @@
+package br.gov.softwarepublico.teste.modelo;
+
+public class ImovelTestePedro {
+	
+	private String matricula;
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	
+}
